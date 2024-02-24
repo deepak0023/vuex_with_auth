339bb990574f1f872067667a00be381181407146
deepak <deepamdeebu@gmail.com>
2024-02-24 12:13:23 +0530

    Initialize : Initialize vue js project with basic signup and login and home page components

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vuex_with_auth
-This is to check the vuex and auth functionality
+# my-blog
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div v-for="blog in blogs" :key="blog.id">
+      <div class="blog">
+        <h3>{{ blog.title }}</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          aspernatur consectetur doloremque sunt ducimus enim iure animi fugit
+          nulla et! Perferendis autem deleniti quo eum corrupti reiciendis
+          voluptatem ab ducimus?
+        </p>
+        <div class="icons">
+          <span>upvote or downvote this article: </span>
+          <span class="material-icons">thumb_up</span>
+          <span class="material-icons">thumb_down</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const blogs = ref([
+      { title: "Topic 1", id: 1 },
+      { title: "Topic 2", id: 2 },
+      { title: "Topic 3", id: 3 },
+    ]);
+
+    return {
+      blogs,
+    };
+  },
+};
+</script>
